c39e3c3ddec160a0f894ebf9f0445db664c27b2f
Yi Fan Goh <gohyf91@gmail.com>
2021-01-05 19:54:30 +0800

    add error catch and nodemon

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 
 app.get('/api/search', async(req, res) => {
   try {
-    const response = await axios.get(`https://api.github.com/search/repositories?q=${req.query.q}`);
+    const response = await axios.get("https://api.github.com/search/repositories?q=");
     const arr = response.data.items.slice(0, 5);
     const res_array = [];
     arr.forEach((repo) => {
@@ -17,7 +17,11 @@ app.get('/api/search', async(req, res) => {
     })
     res.status(200).json(res_array);
   } catch (e) {
-    res.status(500).json([]);
+    console.log(e.response.status);
+    console.log(e.response.statusText);
+    const statusCode = e.response.status;
+    const statusText = e.response.statusText;
+    res.status(statusCode).send(statusText);
   }
 });
 
